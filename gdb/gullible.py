7757b2b2a1cd0cc2854a2cdd41a9b8c40dd104f4
Josh Bressers <josh@bress.net>
2012-10-01 06:06:39 -0500

    Add skeleton files

---
+++ gdb/gullible.py
@@ -0,0 +1,32 @@
+# Copyright 2012 Josh Bressers <josh@bress.net>
+#
+# This file is part of gullible.
+# 
+# gullible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# gullible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with gullible.  If not, see <http://www.gnu.org/licenses/>.
+
+import gdb
+import gullible
+
+class Gullible(gdb.Command):
+    "Analyze a crash to determine exploitability."
+
+    def __init__(self):
+        super (Gullible, self).__init__("gullible", gdb.COMMAND_SUPPORT,
+                                gdb.COMPLETE_NONE, True)
+
+    def invoke(self, arg, from_tty):
+        "Called when the command is invoked from GDB."
+        pass
+
+Gullible()
