5e2e6ad53da62d3a0179b29719db6df3190931a2
Josh Bressers <josh@bress.net>
2012-10-01 05:46:09 -0500

    Add some basic tests

---
+++ tests/testWriteRandom.c
@@ -0,0 +1,14 @@
+/*
+ * Test a crash attempting to write invalid memory
+ * This error is exploitable
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    char *a;
+    a = malloc(1024);
+    a = (size_t)a * 1024;   // This should put us well outside the valid memory range
+    strcpy(a, "pwnt");
+}
